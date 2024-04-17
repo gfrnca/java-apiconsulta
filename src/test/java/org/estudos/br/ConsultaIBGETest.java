7272dc1c6fac79704f677540e6f85907f729b9d3
Paulo Roberto <evans_sp@hotmail.com>
2024-04-17 00:22:22 -0300

    Add unit test

--- src/test/java/org/estudos/br/ConsultaIBGETest.java
+++ src/test/java/org/estudos/br/ConsultaIBGETest.java
@@ -1,12 +1,8 @@
 package org.estudos.br;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
