cc05a7e32e9db6546bd2432043ba54765df4ab38
Paulo Roberto <evans_sp@hotmail.com>
2024-04-17 00:03:15 -0300

    Add unit test with Mockito

--- src/test/java/org/estudos/br/ConsultaIBGEMockTest.java
+++ src/test/java/org/estudos/br/ConsultaIBGEMockTest.java
@@ -10,11 +10,12 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
-import java.net.URL;
+
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
-public class ConsultaIBGETest {
+public class ConsultaIBGEMockTest {
 
     // Mock para simular a conexão HTTP
     @Mock
@@ -34,6 +35,7 @@ public class ConsultaIBGETest {
 
     // Teste para verificar se o método consultarEstado retorna o JSON esperado para o estado de São Paulo
     @Test
+    @DisplayName("Consulta usando o Mock")
     public void testConsultarEstadoComMock() throws IOException {
         // Sigla do estado a ser consultado
         String estadoUf = "SP";
@@ -44,24 +46,5 @@ public class ConsultaIBGETest {
         // Verificamos se o JSON retornado é o mesmo que o JSON de resposta simulada
         assertEquals(JSON_RESPONSE, response, "O JSON retornado não corresponde ao esperado.");
     }
-
-    @Test
-    @DisplayName("Pegando o Status Code")
-    public void testConsultarEstadoStatusCode() throws IOException {
-
-        // Sigla do estado a ser consultado
-        String estadoUf = "SP";
-
-        // Cria uma conexão HTTP com a URL da API do IBGE para consultar informações sobre o estado "SP" vindo da variável {estadoUf}
-        HttpURLConnection connection = (HttpURLConnection) new URL("https://servicodados.ibge.gov.br/api/v1/localidades/estados/" + estadoUf).openConnection();
-        // Define o método da requisição como GET
-        connection.setRequestMethod("GET");
-        // Obtém o status code da resposta da API
-        int statusCode = connection.getResponseCode();
-
-        // Verifica se o status code retornado é igual a 200 (OK) e dá uma msg de erro se não for
-        assertEquals(200, statusCode, "O status code da resposta não é 200.");
-    }
-
 
 }
